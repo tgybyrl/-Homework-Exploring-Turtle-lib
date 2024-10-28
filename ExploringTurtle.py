712ac07b962a3ce2f58b94a473f604759f2fb54f
Tugay Bayırlı <tgybyrl48@gmail.com>
2024-10-28 18:11:56 +0200

    Exploring Turtle lib

---
+++ ExploringTurtle.py
@@ -0,0 +1,53 @@
+import turtle
+from turtle import *
+print(range(50))
+
+s = turtle.getscreen()
+
+
+forward(200)
+left(120)
+print(turtle.pos())
+forward(200)
+forward(200)
+print(turtle.pos())
+left(120)
+forward(400)
+print(turtle.pos())
+left(120)
+forward(200)
+
+print(turtle.pos())
+
+#for steps in range(10):
+    #for c in ('red','yellow','black'):
+        #color(c)
+        #forward(steps)
+        #right(40)
+
+#while True:
+for steps in range(2):
+    pass
+    for c in ("red","blue"):
+        color(c)
+        forward(200)
+        right(120)
+        forward(200)
+        right(120)
+        forward(200)
+        left(60)
+        forward(200)
+        left(120)
+        forward(200)
+        left(120)
+        forward(200)
+        right(60)
+
+for steps in range(100):
+    pass
+    for c in ('red','yellow','black'):
+        color(c)
+        forward(steps)
+        right(40)
+
+
